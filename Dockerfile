b30b49eb4bca8711ba10afad165f235a88972f5c
KAjalshahi123 <shahikajal274508@gmail.com>
2026-07-05 19:09:44 +0530

    init

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+#FROM ubuntu:latest
+#LABEL authors="kajal"
+FROM eclipse-temurin:21-jdk
+
+WORKDIR /app
+
+COPY target/demo-0.0.1-SNAPSHOT.jar app.jar
+
+EXPOSE 8082
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
+
+
+#FROM eclipse-temurin:21-jdk
+#
+#WORKDIR /app
+#
+#COPY target/*.jar app.jar
+#
+#EXPOSE 8080
+#
+#ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/main/java/com/CICD/demo/controller/WelcometoCOntroller.java
@@ -0,0 +1,17 @@
+package com.CICD.demo.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/welcome")
+public class WelcometoCOntroller {
+
+    @GetMapping
+    public ResponseEntity<String> welcome(){
+        return ResponseEntity.ok("Welcome to CICD");
+    }
+}
